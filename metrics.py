811298b7d65363f96a47aa2c6f3ee39ce9537a1e
Grigoriy Andeev <griha.andreev@gmail.com>
2020-03-18 14:18:51 +0200

    metrics script upload

---
+++ metrics.py
@@ -0,0 +1,34 @@
+# python3 gathering metrics script
+
+import sys
+import psutil
+
+# check args from command line
+if len (sys.argv) < 2:
+    print ('wrong argument, will try "metrics cpu" or "metrics mem" commands')
+# if arg = cpu, print CPU metrics:
+elif sys.argv[1] == "cpu":
+    cpu = psutil.cpu_times()
+    print("CPU LOADING INFO:")
+    print('cpu.idle: ', cpu[3])
+    print('cpu.user: ', cpu[0])
+    print('cpu.guest: ', cpu[8])
+    print('cpu.iowait: ', cpu[4])
+    print('cpu.steal: ', cpu[7])
+    print('cpu.system: ', cpu[2])
+# if arg = mem, print memory  metrics:
+elif sys.argv[1] == "mem":
+    mem = psutil.virtual_memory()
+    swap = psutil.swap_memory()
+    print("MEMORY LOADING INFO:")
+    print('mem.total: ', mem[0])
+    print('mem.used: ', mem[3])
+    print('mem.free: ', mem[4])
+    print('mem.shared: ', mem[9])
+    print('swap.total: ', mem[0])
+    print('swap.used: ', mem[1])
+    print('swap.free: ', mem[2])
+# other way print help
+else:
+    print ('wrong argument, will try "metrics cpu" or "metrics mem" commands')
+    
